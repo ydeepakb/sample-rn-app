fe2dfc4145201da70f31c4ba7a125276e094cd26
ydeepakb <ydeepakb@gmail.com>
2025-12-18 23:32:27 +0000

    color update

--- sample-rn-app/app/(tabs)/_layout.tsx
+++ sample-rn-app/app/(tabs)/_layout.tsx
@@ -12,9 +12,11 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+          //tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
         tabBarButton: HapticTab,
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
       }}>
       <Tabs.Screen
         name="index"
@@ -26,7 +28,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'open',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
--- sample-rn-app/app/(tabs)/explore.tsx
+++ sample-rn-app/app/(tabs)/explore.tsx
@@ -100,7 +100,7 @@ export default function TabTwoScreen() {
 
 const styles = StyleSheet.create({
   headerImage: {
-    color: '#808080',
+    color: '#075a2aff',
     bottom: -90,
     left: -35,
     position: 'absolute',
